89e147faffefc5afa5af75b4ef8202404fa44780
AlexandraDobrescu <alexandra.dobrescu@asaff.ro>
2017-04-05 20:06:42 +0300

    Update time_server.py

--- TP3/time_server.py
+++ TP3/time_server.py
@@ -7,26 +7,59 @@ Ce serveur accepte 5 clients concurents et indique l'heure de serveur.
 """
 import socket
 import time
-
-# create a socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import sys
 
 # get local machine name
 host = socket.gethostname()
-
 port = 6666
 
-# bind to the port
-serversocket.bind((host, port))
+#Apres ca on peut demarer le serveur
 
+# create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# bind to the port
+server_address =(host, port)
+serversocket.connect((server_address))
 # queue up to 5 requests
 serversocket.listen(5)
+ 
+print "Le serveur marche :) "
+
+def client_envoi():
+        while True:
+                message = raw_input("Text: ")
+                serversocket.send(message)
+ 
+def client_recieve():
+        while True:
+                reply = serversocket.recv(1024)
+                print "recu", repr(reply)
+ 
+ 
+thread_send = []
+thread_rcv = []
+num_threads = 5
+ 
+for boucle1 in range(num_threads):
+        thread_send.append(threading.Thread(target = client_envoi))
+        thread_send[-1].start()
+ 
+for boucle2 in range(num_threads):
+        thread_rcv.append(threading.Thread(target = client_recieve))
+        thread_rcv[-1].start()
+
 
 while True:
     # establish a connection
+    
     clientsocket,addr = serversocket.accept()
-
     print("Connection de client avec IP %s" % str(addr))
+    
     currentTime = time.ctime(time.time()) + "\r\n"
     clientsocket.send(currentTime.encode('ascii'))
+
+    temps = serversocket.recv(1024)
+    print time.ctime(time.time()) + str(addr) + str(temps)
+    
     clientsocket.close()
+    
